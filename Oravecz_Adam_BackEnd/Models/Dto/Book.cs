f9525f1b5fc3ce920daa16067ed702a641e75d3d
Oravecz Adam <oravecza@kkszki.hu>
2026-01-09 09:11:16 +0100

    T

--- Oravecz_Adam_BackEnd/Models/Dto/Book.cs
+++ Oravecz_Adam_BackEnd/Models/Dto/Book.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace Oravecz_Adam_BackEnd.Models;
 
@@ -15,7 +17,17 @@ public partial class Book
 
     public int? CategoryId { get; set; }
 
+    [JsonIgnore]
     public virtual Author? Author { get; set; }
 
+    [JsonIgnore]
     public virtual Category? Category { get; set; }
+}
+
+public static class StartupExtensions
+{
+    public static void AddCustomJsonOptions(this IServiceCollection services)
+    {
+        services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+    }
 }
--- Oravecz_Adam_BackEnd/obj/Debug/net8.0/Oravecz_Adam_BackEnd.AssemblyInfo.cs
+++ Oravecz_Adam_BackEnd/obj/Debug/net8.0/Oravecz_Adam_BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oravecz_Adam_BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a0273e91732c2427e8540ce474b18d2d6fd34c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oravecz_Adam_BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oravecz_Adam_BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
